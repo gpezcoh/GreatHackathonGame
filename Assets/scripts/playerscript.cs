ba134784d6d1c40dbf4112a44db34a191ec41917
Gabriel Pezanoski-Cohen <gpezcoh@dhcp-10-105-135-74.wireless.northwestern.private>
2016-11-18 22:54:31 -0600

    it moves

---
+++ Assets/scripts/playerscript.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playerscript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 temp = transform.position;
+		if (Input.GetKeyDown("right")) {
+			temp.x += 10;
+		}
+		else if (Input.GetKeyDown("left")) {
+			temp.x -= 10;
+		}
+		else if (Input.GetKeyDown("up")) {
+			temp.z += 10;
+		}
+		else if (Input.GetKeyDown("down")) {
+			temp.z -= 10;
+		}
+		transform.position = temp;
+	}
+}
